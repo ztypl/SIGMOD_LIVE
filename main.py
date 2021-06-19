eb37c05d06ad83dfb2e5b0b9c4d39b3450b90849
wenbo <li-wb17@mails.tsinghua.edu.cn>
2021-06-20 07:52:38 +0800

    Delete last line and send room id

--- main.py
+++ main.py
@@ -38,29 +38,30 @@ info = read_info("data/info.json")
 set_proxy(info['proxy_host'])
 
 while True:
+    try:
+        if not have_cookie_file():
+            print('无Cookies文件，首次需要手动登录')
+            manually_login()
+        status, browser = auto_login()
 
-    if not have_cookie_file():
-        print('无Cookies文件，首次需要手动登录')
-        manually_login()
-    status, browser = auto_login()
+        if not status:
+            print('自动登录失败，请手动删除data/cookies.json后重试或联系管理员')
+            exit(1)
 
-    if not status:
-        print('自动登录失败，请手动删除data/cookies.json后重试或联系管理员')
-        exit(1)
+        print("1. 获取YouTube直播网页")
+        youtube_webpage = get_youtube_live_webpage(info['channel_id'], info['google_api_key'])
+        if not youtube_webpage:
+            send(info['room_id'], "未查询到Youtube直播间")
 
-    print("1. 获取YouTube直播网页")
-    youtube_webpage = get_youtube_live_webpage(info['channel_id'], info['google_api_key'])
-    if not youtube_webpage:
-        send(1, "未查询到Youtube直播间")
+        print("2. 打开bilibili直播间")
+        push_link = get_bilibili_live_info(browser)
 
-    print("2. 打开bilibili直播间")
-    push_link = get_bilibili_live_info(browser)
-
-    print("3. 获取YouTube直播流")
-    youtube_link = get_youtube_live_info(youtube_webpage)
-    if not youtube_link:
-        send(3, "无法获取到直播流，请检查代理后重试")
-
+        print("3. 获取YouTube直播流")
+        youtube_link = get_youtube_live_info(youtube_webpage)
+        if not youtube_link:
+            send(info['room_id'], "无法获取到直播流，请检查代理后重试")
+    catch Exception:
+        send(info['room_id'], '未知错误')
     try:
         print("4. 开始推流")
         for line in push_livestream(youtube_link, push_link):
@@ -76,4 +77,4 @@ while True:
         continue
 
     # exit(0)
-    send(0, '脚本退出')
+    # send(0, '脚本退出')
